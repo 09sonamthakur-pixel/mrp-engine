85e17ab1f803faa92527ba4b9f6563e90fa76d0f
Your Name <your_email@gmail.com>
2026-06-15 10:25:14 +0530

    MRP Engine project update

--- mrp-engine/src/main/java/dto/ItemDto.java
+++ mrp-engine/src/main/java/dto/ItemDto.java
@@ -1,6 +1,30 @@
 package dto;
 
 public class ItemDto {
+    private Long itemId;
      private String itemName;
     private String description;
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getdescription() {
+        return description;
+    }
+
+    public void setdescription(String description) {
+        this.description = description;
+    }
 }
